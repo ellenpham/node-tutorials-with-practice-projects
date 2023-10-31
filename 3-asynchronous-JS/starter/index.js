c0eb7f244b2e6b424503e4b4f08d979226622c16
ellenpham <ellenpham1510@gmail.com>
2023-11-01 05:46:51 +1100

    Complete Asynchronous JS

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,6 +19,63 @@ const writeFilePromise = (file, data) => {
     });
 };
 
+// using promises with async/await (JS ES8)
+// an async function always returns a promise 
+// and the value that we return from the async function will be the result of that promise
+const getDogPic = async () => {
+    try {
+        const data = await readFilePromise(`${__dirname}/dog.txt`);
+        console.log(`Breed: ${data}`);
+
+        const res1Promise = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+        const res2Promise = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+        const res3Promise = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+        const all = await Promise.all([res1Promise, res2Promise, res3Promise]);
+        const imgs = all.map(el => el.body.message);
+
+        console.log(imgs);
+
+        await writeFilePromise('dog-img.txt', imgs.join('\n'));
+        console.log('Random dog image saved to file!')
+
+    } catch (err) {
+        console.log(err);
+        throw(err);
+    }
+
+    return '2: READY!';
+};
+
+( async () => {
+    try {
+        console.log('1: Will get dog pics!');
+        const x = await getDogPic()
+        console.log(x)
+        console.log('3: Done getting dog pics');
+
+    } catch (err) {
+        console.log('ERROR!!!')
+    }
+})();
+
+/*
+console.log('1: Will get dog pics!');
+getDogPic()
+.then( x => {
+    console.log(x);
+    console.log('3: Done getting dog pics');
+})
+.catch( err => {
+    console.log('ERROR!!!')
+});
+*/
+
+
+// using promises with the then() method
+/*
 readFilePromise(`${__dirname}/dog.txt`)
 .then( (data) => {
     console.log(`Breed: ${data}`);
@@ -34,4 +91,4 @@ readFilePromise(`${__dirname}/dog.txt`)
 .catch( (err) => {
     console.log(err.message);
 });
-
+*/
