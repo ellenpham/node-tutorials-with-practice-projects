da973ed0e84fefae25871d1c711ff5e882e43e0b
ellenpham <ellenpham1510@gmail.com>
2024-01-04 21:20:56 +1100

    refactor code for MVC structure

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,84 +1,119 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require('../models/tourModel');
 
 // Read tours data from tours-simple.json
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+// );
 
 // Check if the id exists
-const checkID = (req, res, next, val) => {
-  // eslint-disable-next-line no-console
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// const checkID = (req, res, next, val) => {
+//   // eslint-disable-next-line no-console
+//   console.log(`Tour id is: ${val}`);
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+//   next();
+// };
 
 // Create a checkBody middleware
 // Check if body contains the name and price property
 // If not, send back 400 bad request
 // Add it to the post handler stack
-const checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      messsage: 'Missing required fields',
-    });
-  }
-  next();
-};
+// const checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       messsage: 'Missing required fields',
+//     });
+//   }
+//   next();
+// };
+
+// Test controllers using data saved in local storage
+
+// const getAllTours = (req, res) => {
+//   res.status(200).json({
+//     status: 'Success',
+//     requestedAt: req.requestTime,
+//     results: tours.length,
+//     data: {
+//       tours,
+//     },
+//   });
+// };
+
+// const getTour = (req, res) => {
+// Convert id from string to integer
+// const id = req.params.id * 1;
+// Use find() method to find the tour with id=id in the tours array
+// const tour = tours.find((el) => el.id === id);
+// res.status(200).json({
+//   status: 'Success',
+//   data: {
+//     tour,
+//   },
+// });
+//};
+
+// const createTour = (req, res) => {
+//   // define the id of the new tour
+//   const newId = tours[tours.length - 1].id + 1;
+//   // create the new tour
+//   const newTour = { id: newId, ...req.body };
+//   // add the new tour to the tours array
+//   tours.push(newTour);
+//   // write into the tours-simple.json file
+//   fs.writeFile(
+//     `${__dirname}/dev-data/data/tours-simple.json`,
+//     JSON.stringify(tours),
+//     // eslint-disable-next-line no-unused-vars
+//     (err) => {
+//       res.status(201).json({
+//         status: 'Success',
+//         data: {
+//           tour: newTour,
+//         },
+//       });
+//     },
+//   );
+// };
+
+// Controllers interacting with real database
 
 const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'Success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+  res.status.json({
+    message: 'success',
   });
 };
 
 const getTour = (req, res) => {
-  // Convert id from string to integer
-  const id = req.params.id * 1;
-
-  // Use find() method to find the tour with id=id in the tours array
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      tour,
-    },
+  res.status.json({
+    message: 'success',
   });
 };
 
-const createTour = (req, res) => {
-  // define the id of the new tour
-  const newId = tours[tours.length - 1].id + 1;
-  // create the new tour
-  const newTour = { id: newId, ...req.body };
-  // add the new tour to the tours array
-  tours.push(newTour);
-  // write into the tours-simple.json file
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    // eslint-disable-next-line no-unused-vars
-    (err) => {
-      res.status(201).json({
-        status: 'Success',
-        data: {
-          tour: newTour,
-        },
-      });
-    },
-  );
+const createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      message: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 const updateTour = (req, res) => {
@@ -103,6 +138,6 @@ module.exports = {
   createTour,
   updateTour,
   deleteTour,
-  checkID,
-  checkBody,
+  //checkID,
+  //checkBody,
 };
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -18,43 +18,6 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-// Define tour schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// Create Tour model from tourSchema
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Create a tour instance from Tour model
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-// Save the test tour to the tours collection
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR: ', err);
-  });
-
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
