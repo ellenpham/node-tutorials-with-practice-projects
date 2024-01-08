df16befb55ff98187be2448709d0a80f380f2ed6
ellenpham <ellenpham1510@gmail.com>
2024-01-08 16:01:09 +1100

    refactor API features

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 // const fs = require('fs');
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 // Read tours data from tours-simple.json
 // const tours = JSON.parse(
@@ -97,75 +98,84 @@ const aliasTopTours = (req, res, next) => {
 
 const getAllTours = async (req, res) => {
   try {
-    console.log(req.query); // { difficulty: 'easy', page: '2', sort: '1', limit: '10' }
+    console.log(req.query);
     // BUILD QUERY
     // 1A) Filtering
     // example url: http://localhost:8000/api/v1/tours?difficulty=easy&page=2&sort=1&limit=10
 
     // create a shallow copy of req.query
-    const queryObj = { ...req.query };
-    // define the excluded fields for filtering just the difficulty level
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    // loop over the excludedFileds array and delete each element of excludedFileds that exists in the queryObj
-    excludedFields.forEach((el) => delete queryObj[el]);
-    // console.log(queryObj); // { difficulty: 'easy' }
+    // const queryObj = { ...req.query };
+    // // define the excluded fields for filtering just the difficulty level
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // // loop over the excludedFileds array and delete each element of excludedFileds that exists in the queryObj
+    // excludedFields.forEach((el) => delete queryObj[el]);
+    // // console.log(queryObj); // { difficulty: 'easy' }
 
-    // 1B) Advanced filtering
-    // example url: http://localhost:8000/api/v1/tours?duration[gte]=5&difficulty=easy&price[lt]=1500
-    let queryStr = JSON.stringify(queryObj);
-    // console.log(queryObj); // { duration: { gte: '5' }, difficulty: 'easy', price: { lt: '1500' } }
-    // console.log(queryStr); // {"duration":{"gte":"5"},"difficulty":"easy","price":{"lt":"1500"}}
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-    // {
-    //   duration: { '$gte': '5' },
-    //   difficulty: 'easy',
-    //   price: { '$lt': '1500' }
-    // }
+    // // 1B) Advanced filtering
+    // // example url: http://localhost:8000/api/v1/tours?duration[gte]=5&difficulty=easy&price[lt]=1500
+    // let queryStr = JSON.stringify(queryObj);
+    // // console.log(queryObj); // { duration: { gte: '5' }, difficulty: 'easy', price: { lt: '1500' } }
+    // // console.log(queryStr); // {"duration":{"gte":"5"},"difficulty":"easy","price":{"lt":"1500"}}
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // // console.log(JSON.parse(queryStr));
+    // // {
+    // //   duration: { '$gte': '5' },
+    // //   difficulty: 'easy',
+    // //   price: { '$lt': '1500' }
+    // // }
 
-    let query = Tour.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
     // example url: http://localhost:8000/api/v1/tours?sort=price --> sort in ascending order
     // example url: http://localhost:8000/api/v1/tours?sort=-price --> sort in descending order
     // example url: http://localhost:8000/api/v1/tours?sort=-price,ratingsAvarage
     // or http://localhost:8000/api/v1/tours?sort=-price,-ratingsAverage
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      // console.log(req.query.sort); -price,-ratingsAverage
-      // console.log(sortBy); // -price -ratingsAverage
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('_id');
-      // default sorting --> change from sort by createdAt to sort by id as it causes conflicts with pagination
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   // console.log(req.query.sort); -price,-ratingsAverage
+    //   // console.log(sortBy); // -price -ratingsAverage
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort('_id');
+    //   // default sorting --> change from sort by createdAt to sort by id as it causes conflicts with pagination
+    // }
 
     // 3) Field limiting
     // example url: http://localhost:8000/api/v1/tours?fields=name,duration,difficulty,price
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v');
+    // }
 
     // 4) Pagination
     // example url: http://localhost:8000/api/v1/tours?page=2&limit=3
-    const page = req.query.page * 1 || 1; // to convert string to number
-    const limit = req.query.limit * 1 || 100; // the number of results to be displayed in each page
-    const skip = (page - 1) * limit;
-    // page=2&limit=10, page 1: 1-10, page 2: 11-20, page 3: 21-30, etc.
-    // when we need to jump to a particular page, it means we need to skip the previous pages
-    // and it is calculated as the previous page times the number of results on each page "skip = (page - 1) * limit"
-    query = query.skip(skip).limit(limit);
+    // const page = req.query.page * 1 || 1; // to convert string to number
+    // const limit = req.query.limit * 1 || 100; // the number of results to be displayed in each page
+    // const skip = (page - 1) * limit;
+    // // page=2&limit=10, page 1: 1-10, page 2: 11-20, page 3: 21-30, etc.
+    // // when we need to jump to a particular page, it means we need to skip the previous pages
+    // // and it is calculated as the previous page times the number of results on each page "skip = (page - 1) * limit"
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
     // EXECUTE QUERY
-    const tours = await query;
+    // create a new object of APIFeatures class with 2 arguments being passed in
+    // Tour.find() is the query object, req.query is the query string that comes from Express
+    // then call the methods to manipulate the query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    // await the result of the query
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
