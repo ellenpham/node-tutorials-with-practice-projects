eca923f54a551dbcc372908d8500deb40605c09a
ellenpham <ellenpham1510@gmail.com>
2024-01-11 15:57:14 +1100

    handle unhandled rejection and uncaught exception

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -2,6 +2,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCCEPTION! Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -16,11 +22,19 @@ mongoose
   .then(() => {
     console.log('DB connection successful!');
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error('Error connecting to database: ', err));
 
 // Start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
