10717d571ad94e32a1efde1b3b75d9cc95849b18
ellenpham <ellenpham1510@gmail.com>
2024-01-13 17:25:21 +1100

    implement login feature

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: [8, 'A password must have more or equal than 8 characters'],
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,14 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+// create an instance methods and is available on all user documents
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 // Create User model from userSchema
 const User = mongoose.model('User', userSchema);
